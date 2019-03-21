a998ecf25289a4434e56582141c20811373ec7c1
sahininayanthara <sahininayanthara95@gmail.com>
2019-03-21 08:28:27 +0530

    first commit

---
+++ length.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<?php
+echo strlen("hello world");
+echo "<br>";
+echo str_word_count("hello world");
+echo "<br>";
+echo str_replace("Hi", "Hello", "Hi Daniel");
+echo "<br>";
+echo strrev("Hi Daniel");
+echo "<br>";
+echo 8%5;
+echo "<br>";
+echo 5**3;
+?>
+
+
+
+</body>
+</html>
